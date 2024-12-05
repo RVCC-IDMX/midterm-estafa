1d09db8b6b98037fe64bed0c6386549195bc9210
Estafa <whalefancypants@gmail.com>
2024-12-05 15:18:00 -0500

    fixed hovering elements in flavors section

--- script.js
+++
@@ -1,9 +0,0 @@
-const store1 = document.getElementById('test');
-var store2 = document.getElementById("test2");
-const btn = document.getElementById("button");
-
-document.getElementById("button").onclick = function() {
-    console.log(`${store1} ${store1.length}`);
-};
-
-
