8c883768a2957ecfb3b74581d17d7d613cf11f0e
Estafa <whalefancypants@gmail.com>
2024-12-04 20:33:06 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const store1 = document.getElementById('test');
+var store2 = document.getElementById("test2");
+const btn = document.getElementById("button");
+
+document.getElementById("button").onclick = function() {
+    console.log(`${store1} ${store1.length}`);
+};
+
+
